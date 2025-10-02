3c78f9c82f602ca441421d7f60b5be40626b901a
aoi.ikeda <skmaoi.000@icloud.com>
2025-10-03 01:05:01 +0900

    ソース修正

--- src/work3/sampleSpellcheck.ts
+++ src/work3/sampleSpellcheck.ts
@@ -2,11 +2,11 @@
 // This function calculate the sum of numbers and print the reslt.
 
 function calculateSum(numbers: number[]) {
-    let total = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      total += numbers[i];
-    }
-    return total
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
   }
-  
+  return total;
+}
+
 calculateSum([10, 20, 30]);
